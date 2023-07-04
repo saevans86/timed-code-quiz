1749c52e19fe6d42d335f6fa34cf13f1fef71d23
saevans86 <sam_evans1986@hotmail.com>
2023-07-04 15:25:16 -0600

    a little formating, wrote the questions to the timed quiz

---
+++ script.js
@@ -0,0 +1,22 @@
+// String Values must be enclosed w?thin ______
+// when being assigned to variables?
+// 1. commas
+// 2. curly brackets
+// 3. quotes @
+// 4. parantheses
+// 
+// HTML stands for Hypertext Markup Language
+// 1. True
+// 2. False
+// 
+// 
+// A Data Structure is Is a storage format that defines the way data is stored, organized, and manipulated.
+// 1. True
+// 2. False
+// 
+// A boolean is used to determine true or false
+// 1. true
+// 2. false
+// 
+// 
+
