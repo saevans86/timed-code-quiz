372b4fda2715eb11a22066b58030d8af582db07f
saevans86 <sam_evans1986@hotmail.com>
2023-07-05 17:17:22 -0600

    updated some of the formating to make it easier to to add boxing/styling,questions updated in js file, and css file updated to reflect changes made in html file.

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
-// String Values must be enclosed w?thin ______
+let questions = [     {
+    question:1,
+    question: "String Values must be enclosed within ______", 
+answer: "quotes", 
+options: ["commas", "curly brackets", "quotes", "parantheses"]
+}, 
+{
+question:2,    
+question: "A Data Structure is Is a storage format that defines the way data is stored, organized, and manipulated.",
+answer: "true", 
+options: ["true", "false"]
+}, 
+{
+question:3,    
+question: "A boolean is used to determine true or false", 
+answer: "true",
+options: ["true", "false"]
+    
+}, 
+{
+    question:4,    
+question: "An array in JavaScript is a type of global object that is used to store data.",
+answer: "true",
+options: ["true", "false"]
+}]
+
+
+// console.log(questions[3].answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// String Values must be enclosed within ______
 // when being assigned to variables?
 // 1. commas
 // 2. curly brackets
@@ -19,4 +80,5 @@
 // 2. false
 // 
 // 
+
 
