d556a64935d200410176c468a9ae76694402afed
saevans86 <sam_evans1986@hotmail.com>
2023-07-06 14:11:08 -0600

    additional formating adding, worked in the timer, still need to create function for array.

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
-let questions = [     {
-    question:1,
-    question: "String Values must be enclosed within ______", 
-answer: "quotes", 
-options: ["commas", "curly brackets", "quotes", "parantheses"]
-}, 
-{
-question:2,    
-question: "A Data Structure is Is a storage format that defines the way data is stored, organized, and manipulated.",
-answer: "true", 
-options: ["true", "false"]
-}, 
-{
-question:3,    
-question: "A boolean is used to determine true or false", 
-answer: "true",
-options: ["true", "false"]
-    
-}, 
-{
-    question:4,    
-question: "An array in JavaScript is a type of global object that is used to store data.",
-answer: "true",
-options: ["true", "false"]
-}]
+//question strings//
 
+// const questions = [     {
+    // question:1,
+    // question: "String Values must be enclosed within ______", 
+// answer: "quotes", 
+// options: ["commas", "curly brackets", "quotes", "parentheses"]
+// }, 
+// {
+// question:2,    
+// question: "A Data Structure is a storage format that defines the way data is stored, organized, and manipulated.",
+// answer: "true", 
+// options: ["true", "false"]
+// }, 
+// {
+// question:3,    
+// question: "A boolean is used to determine true or false", 
+// answer: "true",
+// options: ["true", "false"]
+    // 
+// }, 
+// {
+    // question:4,    
+// question: "An array in JavaScript is a type of global object that is used to store data.",
+// answer: "true",
+// options: ["true", "false"]
+// }]
 
 // console.log(questions[3].answer)
+
+document.getElementById("beginQuiz").addEventListener("click", function() {
+var timeLeft = 120;
+var timer = setInterval(function 
+    function1() {
+    document.getElementById("timeLeft").innerHTML = timeLeft + "&nbsp" + "seconds remaining";
+    timeLeft -= 1;
+    if(timeLeft <=0) {
+        clearInterval(timer);
+        document.getElementById("timeLeft").innerHTML = "time over"
+    }
+}, 1000);
+// console.log(timeLeft)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
