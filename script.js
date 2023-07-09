1d0bb6edce2dda99dea2bd71d369f18ac5e919cc
saevans86 <sam_evans1986@hotmail.com>
2023-07-09 10:33:57 -0600

    still working on quetions/timer, formatting style to come after

--- script.js
+++ script.js
@@ -1,138 +1,70 @@
 //question strings//
 
-// const questions = [     {
-    // question:1,
-    // question: "String Values must be enclosed within ______", 
-// answer: "quotes", 
-// options: ["commas", "curly brackets", "quotes", "parentheses"]
-// }, 
-// {
-// question:2,    
-// question: "A Data Structure is a storage format that defines the way data is stored, organized, and manipulated.",
-// answer: "true", 
-// options: ["true", "false"]
-// }, 
-// {
-// question:3,    
-// question: "A boolean is used to determine true or false", 
-// answer: "true",
-// options: ["true", "false"]
-    // 
-// }, 
-// {
-    // question:4,    
-// question: "An array in JavaScript is a type of global object that is used to store data.",
-// answer: "true",
-// options: ["true", "false"]
-// }]
+var questionsArray = [     {
+    q:1,
+    question: "What will the following code return: Boolean(10 > 9)", 
+answer: "true", 
+options: ["true", "false"]
+}, 
+{
+q:2,    
+question: "A Data Structure is a storage format that defines the way data is stored, organized, and manipulated.",
+answer: "true", 
+options: ["true", "false"]
+}, 
+{
+q:3,    
+question3: "A boolean is used to determine true or false", 
+answer: "true",
+options: ["true", "false"]
+    
+}, 
+{
+    q:4,    
+question: "An array in JavaScript is a type of global object that is used to store data.",
+answer: "true",
+options: ["true", "false"]
+}]
+
 
 // console.log(questions[3].answer)
-
-document.getElementById("beginQuiz").addEventListener("click", function() {
-var timeLeft = 120;
+//event listener to start the time 
+document.querySelector("#beginQuiz").addEventListener("click", function() {
+var timeLeft = 3;
 var timer = setInterval(function 
-    function1() {
-    document.getElementById("timeLeft").innerHTML = timeLeft + "&nbsp" + "seconds remaining";
+    function1()
+     {
+    document.getElementById("timeLeft").textContent = timeLeft + "seconds remaining";
     timeLeft -= 1;
     if(timeLeft <=0) {
         clearInterval(timer);
-        document.getElementById("timeLeft").innerHTML = "time over"
+        document.getElementById("timeLeft").textContent = "out of time!"
+        // console.log(timeLeft)
     }
 }, 1000);
-// console.log(timeLeft)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// String Values must be enclosed within ______
-// when being assigned to variables?
-// 1. commas
-// 2. curly brackets
-// 3. quotes @
-// 4. parantheses
-// 
-// HTML stands for Hypertext Markup Language
-// 1. True
-// 2. False
-// 
-// 
-// A Data Structure is Is a storage format that defines the way data is stored, organized, and manipulated.
-// 1. True
-// 2. False
-// 
-// A boolean is used to determine true or false
-// 1. true
-// 2. false
-// 
-// 
-
-
+   var startButton =document.getElementById("beginQuiz");
+   var questionsDiv = document.getElementById("quizQuestions");
+   var questionsOpt = document.getElementById("quizOptions");
+
+   if (startButton.style.display === "none") {
+   startButton.style.display = "block";
+} else {
+    startButton.style.display = "none";
+   if 
+    (timeLeft != 0) {
+
+    } else 
+    (timeLeft === 0) 
+        const restartBtn = document.createElement("button");
+        restartBtn.textContent = "Restart Quiz";
+        document.body.appendChild(restartBtn);
+        // need to work out removing "restart quiz" before time is up
+    }
+ 
+}
+
+
+   
+); 
+    
+        
